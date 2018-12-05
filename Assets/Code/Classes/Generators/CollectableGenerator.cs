df0874219643d2b1e2e2db45ca0fb7b40ec2e0e5
mlbors <matyas.bors@gmail.com>
2018-12-05 21:17:38 +0100

    Working on Collectable Generator, need to fix generation and pool

--- Assets/Code/Classes/Generators/CollectableGenerator.cs
+++ Assets/Code/Classes/Generators/CollectableGenerator.cs
@@ -26,10 +26,12 @@ using UnityEngine;
 public class CollectableGenerator : AbstractGenerator<ICollectable>
 {
     /**
+     * @var CollectableType _lastCollectableType type of the last generated object
      * @var Stack _generatedObjects stack of generated objects
      * @var Array _gapes array of the different gapes between each object
      */
 
+    protected CollectableType _lastCollectableType = CollectableType.Death;
     protected Stack<ICollectable> _generatedObjects;
     protected float[] _gapes;
 
@@ -73,10 +75,15 @@ public class CollectableGenerator : AbstractGenerator<ICollectable>
 
     public override void Generate()
     {
+        Debug.Log("::: Generate collectable :::");
+
         _generatedObjects = new Stack<ICollectable>();
 
         CollectableType collectableType = _GetRandomCollectableType();
+        _lastCollectableType = collectableType;
         (_pool as ICollectablePool).NeedType = collectableType;
+
+        Debug.Log(collectableType);
 
         switch(collectableType)
         {
@@ -89,10 +96,10 @@ public class CollectableGenerator : AbstractGenerator<ICollectable>
                 break;
             case CollectableType.PowerUp:
                 break;
+            default:
+                _GenerateBonuses();
+                break;
         }
-
-        _currentObject = _pool.GetObject();
-        (_currentObject as MonoBehaviour).gameObject.SetActive(true);
     }
 
     /**************************************************/
@@ -109,6 +116,11 @@ public class CollectableGenerator : AbstractGenerator<ICollectable>
 
     protected CollectableType _GetRandomCollectableType()
     {
+        if (_lastCollectableType != CollectableType.Bonus)
+        {
+            return CollectableType.Bonus;
+        }
+
         Array values = Enum.GetValues(typeof(CollectableType));
         System.Random random = new System.Random();
         return (CollectableType)values.GetValue(random.Next(values.Length));
@@ -127,6 +139,8 @@ public class CollectableGenerator : AbstractGenerator<ICollectable>
 
     protected void _GenerateBonuses()
     {
+        Debug.Log("::: Generate Bonus :::");
+
         int maxItem;
 
         IPlatform platform = (_referenceObject as IGeneratorOperatorElement<IPlatform>).Generator.CurrentObject;
@@ -157,7 +171,7 @@ public class CollectableGenerator : AbstractGenerator<ICollectable>
         for (int i = 0; i <= amount; i++)
         {
             _currentObject = _pool.GetObject();
-            (_currentObject as MonoBehaviour).transform.position = _ComputePosition(amount, platform);
+            (_currentObject as MonoBehaviour).transform.position = _ComputePosition(amount, platform, i);
             (_currentObject as MonoBehaviour).gameObject.SetActive(true);
             _generatedObjects.Push(_currentObject);
         }
@@ -174,13 +188,14 @@ public class CollectableGenerator : AbstractGenerator<ICollectable>
      * @access protected
      * @param int numberOfItem number of generated objects
      * @param IPlatform platform platform used as a reference
+     * @param int index generation index
      * @return Vector3
      */
 
 
-    protected Vector3 _ComputePosition(int numberOfItem, IPlatform platform)
+    protected Vector3 _ComputePosition(int numberOfItem, IPlatform platform, int index)
     {
-        float xPosition = _ComputeXPosition(numberOfItem, platform);
+        float xPosition = _ComputeXPosition(numberOfItem, platform, index);
         float yPosition = _ComputeYPosition(numberOfItem, platform);
 
         Vector3 position = new Vector3(xPosition, yPosition, (_currentObject as MonoBehaviour).transform.position.z);
@@ -201,35 +216,53 @@ public class CollectableGenerator : AbstractGenerator<ICollectable>
      * @access protected
      * @param int numberOfItem number of generated objects
      * @param IPlatform platform platform used as a reference
+     * @param int index generation index
      * @return float
      */
 
-
-    protected float _ComputeXPosition(int numberOfItem, IPlatform platform)
+    protected float _ComputeXPosition(int numberOfItem, IPlatform platform, int index)
     {
+        Debug.Log("::: Compute x position :::");
+
         float xPosition = 0.00f;
         float offset = 0.00f;
         float platformWidth = (platform as MonoBehaviour).gameObject.GetComponent<BoxCollider2D>().size.x;
         float platformPosition = (platform as MonoBehaviour).transform.position.x - platformWidth / 2;
         float currentObjectWidth = (_currentObject as MonoBehaviour).gameObject.GetComponent<CircleCollider2D>().radius * 2;
 
-        if (_generatedObjects.Count > 0) {
+        // TO DO: fix this and the pool
+        if (_generatedObjects.Count == 0 && index == 0)
+        {
+            float position = 0.00f;
+            float sumOfGapes = 0.00f;
+            float neededSpace = 0.00f;
+
+            _gapes = new float[numberOfItem];
+
+            while (neededSpace == 0.00f || neededSpace > platformWidth)
+            {
+                position = UnityEngine.Random.Range(platformPosition, platformPosition + platformWidth);
+
+                for (int i = 0; i < numberOfItem; i++)
+                {
+                    _gapes[i] = UnityEngine.Random.Range(currentObjectWidth / 2, (platformWidth / numberOfItem) / 10);
+                    sumOfGapes += _gapes[i];
+                }
+
+                neededSpace = position + sumOfGapes + (currentObjectWidth * numberOfItem);
+                Debug.Log(neededSpace);
+            }
+
+            xPosition = position;
+        }
+
+        if (index > 0 && _generatedObjects.Count > 0)
+        {
             ICollectable previousObject = _generatedObjects.Peek();
             float width = (previousObject as MonoBehaviour).gameObject.GetComponent<CircleCollider2D>().radius * 2;
             offset = previousObject.X + width;
-        }
 
-        if (_generatedObjects.Count == 0) {
-            _gapes = new float[numberOfItem];
-            float position = UnityEngine.Random.Range(0, platformPosition + platformWidth);
-
-            for (int i = 0; i < numberOfItem; i++)
-            {
-                _gapes[i] = UnityEngine.Random.Range(currentObjectWidth / 2, (platformWidth / numberOfItem) / 10);
-            }
-
-            //TO DO: check if object will go outside the platform or not
-
+            xPosition = offset + _gapes[index];
         }
 
         return xPosition;
@@ -253,6 +286,10 @@ public class CollectableGenerator : AbstractGenerator<ICollectable>
     protected float _ComputeYPosition(int numberOfItem, IPlatform platform)
     {
         float yPosition = 0.00f;
+        float platformPosition = (platform as MonoBehaviour).transform.position.y;
+
+        yPosition = platformPosition + 40.00f;
+
         return yPosition;
     }
 }
