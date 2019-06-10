841477aec6150865957798bdcb2d59fbf53b108c
mlbors <matyas.bors@gmail.com>
2019-06-10 08:57:28 +0200

    Working on Death Box

---
+++ Assets/Code/Classes/DeathBoxes/DeathBox.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * FTSWFOS - DeathBox - Concrete Class
+ *
+ * @since       2018.01.09
+ * @version     1.0.0.0
+ * @author      MLB
+ * @copyright   -
+ */
+
+/*******************/
+/***** IMPORTS *****/
+/*******************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**************************************************/
+/**************************************************/
+
+/*********************/
+/***** DEATH BOX *****/
+/*********************/
+
+public class DeathBox : AbstractDeathBox
+{
+    /******************************/
+    /***** IDEATH BOX OPERATE *****/
+    /******************************/
+
+    /**
+     * @access public
+     */
+
+    public override void Operate()
+    {
+
+    }
+
+    /**************************************************/
+    /**************************************************/
+
+    /*******************************/
+    /***** ON TRIGGER ENTER 2D *****/
+    /*******************************/
+
+    /**
+     * @access public
+     */
+
+    public override void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        if (otherObject.gameObject.tag == "Player" && otherObject.GetType() == typeof(BoxCollider2D))
+        {
+            Operate();
+        }
+    }
+}
+ 
